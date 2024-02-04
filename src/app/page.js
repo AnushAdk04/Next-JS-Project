4d2189cb0479ed8af6c3491f880c73558389cf2f
AnushAdk04 <adhikarianush64@gmail.com>
2024-02-04 19:59:03 +0545

    header and footers

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
+import { About, Contact, Experiences, HeroSection, Projects, Skills } from "@cct/components";
+import AppFooter from "@cct/components/AppFooter";
+import AppHeader from "@cct/components/AppHeader";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    
+    <AppHeader/>
+    <HeroSection/>
+    <About/>
+    <Skills/>
+    <Projects/>
+    <Experiences/>
+    <Contact/>
+    <AppFooter/>
     </>
   );
 }
