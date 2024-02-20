ef80925eb376b95a1cea4881e047fac62df706e1
AnushAdk04 <adhikarianush64@gmail.com>
2024-02-20 18:31:48 +0545

    digital clock

--- src/app/clock/page.jsx
+++ src/app/clock/page.jsx
@@ -1,40 +1,39 @@
-"use client";
+"use client"
 
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function DigitalClock() {
+    const [time, setTime] = useState(new Date());
 
-    const [time, setTime] = useState()
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
 
-    useEffect(() =>{
-        const intervalId = setInterval(() =>{
-            setTime(new Date())
-        }, 1000)
-
-        return () =>{
+        // Cleanup function to clear the interval
+        return () => {
             clearInterval(intervalId);
-        }
+        };
+    }, []);
 
-    },[])
+    function formatTime() {
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridiem = hours >= 12 ? "PM" : "AM";
 
-    function formatTime(){
-        let hours = time.getHours()
-        const minutes = time.getMinutes()
-        const seconds = time.getSeconds()
-        const meridiem = hours >= 12 ? "PM" : "AM"
+        // Convert hours to 12-hour format
+        hours = hours % 12 || 12;
 
-        hours = hours % 12 || 12
-
-        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
     }
 
-    function padZero(number){
-        return (number < 10 ? "0" : "") + number
+    function padZero(number) {
+        return (number < 10 ? "0" : "") + number;
     }
 
     return (
         <main>
-
             <img src='/images/background.jpg' className="absolute inset-0 w-full h-full object-cover" alt="Background" />
 
             {/* Clock container */}
@@ -42,11 +41,10 @@ export default function DigitalClock() {
                 {/* Clock */}
                 <div className='clock text-black text-7xl font-bold font-serif'>
                     <span>
-                       {formatTime()}
+                        {formatTime()}
                     </span>
                 </div>
             </div>
-            
         </main>
     );
 }
