7d14eb297f8e7cd80074d48f53e39c67e52644ad
AnushAdk04 <adhikarianush64@gmail.com>
2024-02-20 18:22:54 +0545

    digital clock

---
+++ src/app/clock/page.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, {useEffect, useState } from 'react';
+
+export default function DigitalClock() {
+
+    const [time, setTime] = useState()
+
+    useEffect(() =>{
+        const intervalId = setInterval(() =>{
+            setTime(new Date())
+        }, 1000)
+
+        return () =>{
+            clearInterval(intervalId);
+        }
+
+    },[])
+
+    function formatTime(){
+        let hours = time.getHours()
+        const minutes = time.getMinutes()
+        const seconds = time.getSeconds()
+        const meridiem = hours >= 12 ? "PM" : "AM"
+
+        hours = hours % 12 || 12
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`
+    }
+
+    function padZero(number){
+        return (number < 10 ? "0" : "") + number
+    }
+
+    return (
+        <main>
+
+            <img src='/images/background.jpg' className="absolute inset-0 w-full h-full object-cover" alt="Background" />
+
+            {/* Clock container */}
+            <div className='absolute inset-0 flex items-center justify-center'>
+                {/* Clock */}
+                <div className='clock text-black text-7xl font-bold font-serif'>
+                    <span>
+                       {formatTime()}
+                    </span>
+                </div>
+            </div>
+            
+        </main>
+    );
+}
